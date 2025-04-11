4ec500891c6a2d15e1c3dd64a45e87f69bca7173
NAME <EMAIL>
2025-04-11 19:29:47 +0700

    [RED] Add User Model Skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/model/User.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.authprofile.model;
+
+import java.util.UUID;
+
+public class User {
+
+    private UUID userId;
+    private String username;
+    private String password;
+    private String role;
+    private String description;
+    private String phoneNumber;
+    private String fullName;
+
+}
