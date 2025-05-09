7349a800476a8e04af2f678149920fb3360f7e78
NAME <EMAIL>
2025-05-09 16:28:10 +0700

    [GREEN] Implement user model

--- src/main/java/id/ac/ui/cs/advprog/authprofile/model/User.java
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/model/User.java
@@ -1,9 +1,10 @@
 package id.ac.ui.cs.advprog.authprofile.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Generated;
+import org.hibernate.annotations.GenericGenerator;
+import java.util.UUID;
 
 @Entity
 @Table(name = "users")
@@ -11,9 +12,39 @@ import lombok.Generated;
 @Data
 public class User {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name="UUID", strategy="org.hibernate.id.UUIDGenerator")
+    @Column(name="id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "address")
+    private String address;
+
     public User() {
     }
 
     public User(String role, String fullName, String email, String phoneNumber, String password, String address) {
+        this.role = role;
+        this.fullName = fullName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.address = address;
     }
 }
