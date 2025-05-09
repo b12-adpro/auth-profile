9e5ea3fcd45324fa64adaf000e082ee01b884c18
NAME <EMAIL>
2025-05-09 17:20:53 +0700

    [RED] Add skeleton for jwt authentication filter

---
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/security/JwtAuthenticationFilter.java
@@ -0,0 +1,28 @@
+package id.ac.ui.cs.advprog.authprofile.security;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+    }
+}
