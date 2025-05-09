d2fe75a603a8ce409f498756cd4b05f6ed727fef
NAME <EMAIL>
2025-05-09 17:10:05 +0700

    [RED] Add skeleton for jwt provider test

---
+++ src/test/java/id/ac/ui/cs/advprog/authprofile/security/JwtTokenProviderTest.java
@@ -0,0 +1,26 @@
+package id.ac.ui.cs.advprog.authprofile.security;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class JwtTokenProviderTest {
+
+    private JwtTokenProvider jwtTokenProvider;
+    private final String testSecret = "qqFQ/tEeaSlcEwUtG+l30VGNjd+z2BfA5Y5QWsiZWHEFhDgB6kw2YODG9f6cIGn44/DtjmZNxUPH97+YDpR/Ng==";
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testGenerateAndParseToken() {
+    }
+
+    @Test
+    void testValidateToken_Valid() {
+    }
+
+    @Test
+    void testValidateToken_Invalid() {
+    }
+}
