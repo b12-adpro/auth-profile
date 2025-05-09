deeb8ea238b719af8ecc6521a16629e8f1a81b79
NAME <EMAIL>
2025-05-09 17:04:28 +0700

    [GREEN] Implement profile update dto

--- src/main/java/id/ac/ui/cs/advprog/authprofile/dto/ProfileUpdateDto.java
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/dto/ProfileUpdateDto.java
@@ -6,4 +6,10 @@ import lombok.Generated;
 @Generated
 @Data
 public class ProfileUpdateDto {
+    private String role;
+    private String fullName;
+    private String phoneNumber;
+    private String password;
+    private String address;
+    private String bio;
 }
