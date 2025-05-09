60011a42ec0d0a8a94d6296d48cd4636bd8d85e8
NAME <EMAIL>
2025-05-09 18:31:48 +0700

    Revert "[RED] Add profile service skeleton"
    This reverts commit ea49814dbc9854171153fe1abad6a5b40c6aa60c.

--- src/main/java/id/ac/ui/cs/advprog/authprofile/service/ProfileService.java
+++
@@ -1,9 +0,0 @@
-package id.ac.ui.cs.advprog.authprofile.service;
-
-import id.ac.ui.cs.advprog.authprofile.dto.ProfileUpdateDto;
-
-public interface ProfileService {
-    Object updateProfile(ProfileUpdateDto dto, String email, String role) throws Exception;
-
-    Object getProfile(String email, String role) throws Exception;
-}
