e8cff5a3f4bf4761f66105081504390ce2f304a7
NAME <EMAIL>
2025-05-09 20:03:04 +0700

    [REFACTOR] Change from long to UUID for admin repository

--- src/main/java/id/ac/ui/cs/advprog/authprofile/repository/AdminRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/repository/AdminRepository.java
@@ -1,11 +1,14 @@
 package id.ac.ui.cs.advprog.authprofile.repository;
 
-import id.ac.ui.cs.advprog.authprofile.model.Admin;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
+
+import id.ac.ui.cs.advprog.authprofile.model.Admin;
 
 @Repository
-public interface AdminRepository extends JpaRepository<Admin, Long> {
+public interface AdminRepository extends JpaRepository<Admin, UUID> {
     Optional<Admin> findByEmail(String email);
 }
