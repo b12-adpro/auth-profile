4d16b67caea456a256c27633568dd401ee4b9959
NAME <EMAIL>
2025-05-09 16:48:54 +0700

    [GREEN] Implement user repository

--- src/main/java/id/ac/ui/cs/advprog/authprofile/repository/UserRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package id.ac.ui.cs.advprog.authprofile.repository;
 
+import id.ac.ui.cs.advprog.authprofile.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import id.ac.ui.cs.advprog.authprofile.model.User;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
