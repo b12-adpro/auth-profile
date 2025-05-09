5e8cfdb8909c1e08ef6d41f7c2abbf602e2e0bdb
NAME <EMAIL>
2025-05-09 17:25:03 +0700

    [RED] Add test for dot env config

--- src/test/java/id/ac/ui/cs/advprog/authprofile/config/DotenvConfigTest.java
+++ src/test/java/id/ac/ui/cs/advprog/authprofile/config/DotenvConfigTest.java
@@ -1,10 +1,14 @@
 package id.ac.ui.cs.advprog.authprofile.config;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class DotenvConfigTest {
 
     @Test
     void testDotenvBean() {
+        Dotenv dotenv = new DotenvConfig().dotenv();
+        assertNotNull(dotenv, "Dotenv bean should not be null");
     }
 }
