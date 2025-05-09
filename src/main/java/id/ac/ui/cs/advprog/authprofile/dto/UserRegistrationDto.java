096a0c2863dfd706d05abe9ec058531803f6ade6
NAME <EMAIL>
2025-05-09 17:07:33 +0700

    [GREEN] Implement user registration dto

--- src/main/java/id/ac/ui/cs/advprog/authprofile/dto/UserRegistrationDto.java
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/dto/UserRegistrationDto.java
@@ -6,4 +6,10 @@ import lombok.Generated;
 @Generated
 @Data
 public class UserRegistrationDto {
+    private String role;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String address;
 }
