988f4444ef9f2ab22f2132a6a3d3df0b46f9d6c1
NAME <EMAIL>
2025-05-09 17:27:48 +0700

    [GREEN] Implement dot env config

--- src/main/java/id/ac/ui/cs/advprog/authprofile/config/DotEnvConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/config/DotEnvConfig.java
@@ -1,14 +1,16 @@
 package id.ac.ui.cs.advprog.authprofile.config;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import io.github.cdimascio.dotenv.Dotenv;
 
 @Configuration
 public class DotenvConfig {
 
     @Bean
     public Dotenv dotenv() {
+        return Dotenv.configure()
+                .ignoreIfMissing() // in case there's no .env file
+                .load();
     }
 }
