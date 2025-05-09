7208cb8f9594515ae7590e46ea18647cd0c19166
NAME <EMAIL>
2025-05-09 16:33:23 +0700

    [GREEN] Implement admin repository

--- src/main/java/id/ac/ui/cs/advprog/authprofile/repository/AdminRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/repository/AdminRepository.java
@@ -1,10 +1,11 @@
 package id.ac.ui.cs.advprog.authprofile.repository;
 
+import id.ac.ui.cs.advprog.authprofile.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import id.ac.ui.cs.advprog.authprofile.model.Admin;
+import java.util.Optional;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findByEmail(String email);
 }
