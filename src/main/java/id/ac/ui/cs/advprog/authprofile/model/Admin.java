0cfe3916575c2e71d105f03621cc7cb9e844e582
NAME <EMAIL>
2025-05-09 16:04:41 +0700

    [GREEN] Implement admin model

--- src/main/java/id/ac/ui/cs/advprog/authprofile/model/Admin.java
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/model/Admin.java
@@ -1,9 +1,10 @@
 package id.ac.ui.cs.advprog.authprofile.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Generated;
+import org.hibernate.annotations.GenericGenerator;
+import java.util.UUID;
 
 @Entity
 @Table(name = "admins")
@@ -11,9 +12,31 @@ import lombok.Generated;
 @Data
 public class Admin {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name="id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
     public Admin() {
     }
 
     public Admin(String fullName, String email, String phoneNumber, String password) {
+        this.fullName = fullName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
     }
 }
