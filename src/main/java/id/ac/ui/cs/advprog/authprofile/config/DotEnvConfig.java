3daec13f46c5cc65947b9d6d25cf5ccc264606ba
NAME <EMAIL>
2025-05-09 17:25:48 +0700

    [RED] Add skeleton for dot env config

---
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/config/DotEnvConfig.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.authprofile.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@Configuration
+public class DotenvConfig {
+
+    @Bean
+    public Dotenv dotenv() {
+    }
+}
