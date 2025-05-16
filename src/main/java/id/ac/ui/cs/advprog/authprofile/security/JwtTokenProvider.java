e2d82531c354e4afbb4ea76641a7ef94321442d1
NAME <EMAIL>
2025-05-17 04:59:22 +0700

    [REFACTOR] Migrate from dot env to application properties

--- src/main/java/id/ac/ui/cs/advprog/authprofile/security/JwtTokenProvider.java
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/security/JwtTokenProvider.java
@@ -6,6 +6,8 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -14,16 +16,11 @@ import java.util.Date;
 @Component
 public class JwtTokenProvider {
 
-    private final String jwtSecret;
-    private final long JWT_EXPIRATION_MS = 3600000; // 1 hour
-
-    public JwtTokenProvider() {
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        this.jwtSecret = dotenv.get("JWT_SECRET");
-    }
+    @Value("${JWT_SECRET}")
+    private String jwtSecret;
+    private final long JWT_EXPIRATION_MS = 3600000;
 
     private Key getSigningKey() {
-        // Convert Base64-encoded secret into a Key suitable for HS512
         byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
         return Keys.hmacShaKeyFor(keyBytes);
     }
