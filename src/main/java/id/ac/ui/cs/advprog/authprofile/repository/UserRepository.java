70720f871dd959f9734f5dcfcf9fbbffd1c8df77
NAME <EMAIL>
2025-05-17 04:40:35 +0700

    [REFACTOR] Change from long to UUID for userrepository

--- src/main/java/id/ac/ui/cs/advprog/authprofile/repository/UserRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/repository/UserRepository.java
@@ -4,8 +4,9 @@ import id.ac.ui.cs.advprog.authprofile.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 }
